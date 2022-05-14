505756bc73aa586a635a35663a5f62762fd9691c
Taba <elena.tabakov@gmail.com>
2022-05-14 14:03:21 +0300

    Change BG & Size

--- manager/src/App.styles.ts
+++ manager/src/App.styles.ts
@@ -8,8 +8,8 @@ export const Button = styled.button<{newColor: boolean}>`
   ${({newColor}) => newColor ? 'font-size :2rem; background:red' : 'font-size :1rem;background:black; color:#fff'}
 `
 
-export const Cube = styled.div<{size: number; bgColor: string}>`
+export const Cube = styled.div<{size: {height:number, width: number}, bgColor: string}>`
   background: ${({bgColor}) => `${bgColor}`};
-  height: ${({size}) =>  `${size}px`};
-  width: ${({size}) =>  `${size}px`};
+  height: ${({size}) =>  `${size.height}px`};
+  width: ${({size}) =>  `${size.width}px`};
 `
--- manager/src/App.tsx
+++ manager/src/App.tsx
@@ -7,18 +7,16 @@ import './App.css';
 function App() {
 
   const [newColor, setNewColor] = useState(false);
-  const [size, setSize] = useState(50);
+  const [size, setSize] = useState({height : 50 , width : 50});
   const [bgColor, setBgColor] = useState('blue');
 
   const changeSizeHandler = (flag: 'big' | 'small') => {
   if( flag === 'big'){
-    setSize(size*2);
-  } else {
-    setSize(size/2);
+    setSize((prevState) => ({height: prevState.height*2, width : prevState.width*2}) );
+  } else{
+     setSize({height : size.height/2 , width : size.width/2});
   }
-     
-  }
-
+}
   const changeBgCOlorHandler = (bg:string) => {
     setBgColor(bg);
 }
@@ -31,6 +29,7 @@ function App() {
      <button  onClick={() => changeBgCOlorHandler('red')} >Red</button>  <button onClick={() => changeBgCOlorHandler('blue')}>Blue</button><button onClick={() => changeBgCOlorHandler('green')}>Green</button>
      <button  onClick={() => changeSizeHandler('big')} >Change size</button>  <button onClick={() => changeSizeHandler('small')}>Small</button>
      <S.Cube size={size} bgColor={bgColor}/>  
+ 
     </>
   );
 }
